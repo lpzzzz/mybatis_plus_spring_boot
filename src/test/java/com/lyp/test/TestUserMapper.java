1b8c76e904b2ff001af78409e9c4ac7976b717b1
peng.yan.lu <14787005750@163.com>
2020-07-01 10:15:06 +0800

    更新代码

--- src/test/java/com/lyp/test/TestUserMapper.java
+++ src/test/java/com/lyp/test/TestUserMapper.java
@@ -10,14 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Date;
-import java.util.List;
+import javax.annotation.Resource;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestUserMapper {
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     /**
@@ -25,13 +25,14 @@ public class TestUserMapper {
      */
     @Test
     public void insertTest() {
+
         User user = new User();
         user.setAge(12);
         user.setCreateTime(new Date());
         user.setMail("123456@gmail.com");
         user.setManagerId(123456L);
         user.setName("张三");
-        user.setId(12345678L);
+        user.setId(1234568L);
         user.setAddress("中国");
         int result = userMapper.insert(user); // 返回值是一个数据库库被影响的行数
         System.out.println("result => " + result);
@@ -94,7 +95,58 @@ public class TestUserMapper {
     @Test
     public void deleteById() {
         int result = userMapper.deleteById(12345678L);
-        System.out.println("==========删除执行结果========="+ result);
+        System.out.println("==========删除执行结果=========" + result);
     }
 
+
+    /**
+     * 根据Map条件删除数据 ， 多个条件之间使用and连接
+     */
+    @Test
+    public void deleteByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("manager_id", "123456");
+        map.put("age", 12);
+        int result = userMapper.deleteByMap(map);
+        System.out.println("==========执行结果==========" + result);
+    }
+
+    /**
+     * 根据条件进行删除 QueryWrapper
+     */
+    @Test
+    public void deleteByQueryWrapper() {
+        // 方法一 ：
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("name", "张三").eq("id", "1234567");
+        int result = userMapper.delete(wrapper);
+        System.out.println("=========执行结果=========" + result);
+    }
+
+    /**
+     * 根据id进行批量的删除操作
+     */
+    @Test
+    public void testBatchId() {
+        int result = userMapper.deleteBatchIds(Arrays.asList(1234567L, 1234568L));
+        System.out.println("==========执行结果============" + result);
+    }
+
+    /**
+     * 根据主键id进行查询
+     */
+    @Test
+    public void testSelectById() {
+        User user = userMapper.selectById(123456);
+        System.out.println(user);
+    }
+
+    /**
+     * 根据id进行批量查询
+     */
+    @Test
+    public void testSelectByBatchId() {
+        List<User> userList = userMapper.selectBatchIds(Arrays.asList(123456L, 1087982257332887553L));
+        userList.forEach(System.out::println);
+    }
 }
